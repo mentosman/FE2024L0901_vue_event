c539ff45e388b67bcbb31ecab5fc6dc39709c7a9
Acos56 <2301141@s.asojuku.ac.jp>
2024-10-10 11:54:13 +0900

    3-7から3-12作成

--- 3-1/script/script.js
+++ 3-1/script/script.js
@@ -1,9 +1,14 @@
 // JavaScript用ファイル
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            
-        };
-    }
+  el: "#app",
+  data() {
+    return {
+      text: "hello Vue.js",
+    };
+  },
+  methods: {
+    handleClick() {
+      this.text = "button pushed";
+    },
+  },
 });
